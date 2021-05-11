50b1dae88e3fcf4e015bc836033b2c22a5cf6c22
TobsiH <tobias.henning@edu.fhdw.de>
2021-05-11 12:33:24 +0200

    Cleanup

--- server/JavaHttpServer.java
+++ server/JavaHttpServer.java
@@ -18,7 +18,7 @@ import java.util.StringTokenizer;
 //Each Client Connection will be managed in a dedicated thread
 public class JavaHttpServer implements Runnable{
 	
-	static final File WEB_ROOT = new File("C:/Users/Alex/Desktop/FHDW/2. Semester/OPG/Congo/src/");           //file directory
+	static final File WEB_ROOT = new File("C:\\Users\\tobia\\Desktop\\Congo");           //file directory
 	static final String DEFAULT_FILE = "index.html";
 	static final String FILE_NOT_FOUND = "404.html";
 	static final String METHOD_NOT_SUPPORTED = "not_supported.html"; 
